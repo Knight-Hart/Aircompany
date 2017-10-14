3f7c9a1ba15bb52724b742e7b13ad91b3cc8e9d4
Knight-Hart <steveperson666@gmail.com>
2017-10-14 13:20:34 +0300

    123

---
+++ Aircompany/Aircompany/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aircompany
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Airplane.MaxAltitudeAuto = 10000;
+            Airplane.MinAltitudeAuto = 2000;
+            Airplane Plane1 = new Airplane(150, 200, 5);
+            Plane1.SetAltitude(15000);
+            Console.WriteLine(Plane1.Altitude);
+        }
+    }
+}
